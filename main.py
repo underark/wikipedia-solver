188dbef02be1396687fb612a4961502f8a74ac53
alex <peter.alex.morris@gmail.com>
2026-03-06 12:02:02 +0900

    Added logic to continue making requests until all links have been obtained and to return list of complete links

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
 
-userAgentString = ""
 maxDepth = 1
+
+with open("resources/header.txt", "r") as file:
+    userAgentString = file.read().strip()
+    headers = {"User-Agent": userAgentString}
 
 
 def main():
-    print(findTargetPage("Tokyo", "1900 Summer Olympics", 0))
+    result = findTargetPage("MV Mariam", "Merchant ship")
+    print(result)
 
 
-def findTargetPage(title: str, targetPage: str, depth: int):
+def findTargetPage(title: str, targetPage: str, depth=0):
+    print(f"Title is {title} and target is {targetPage} and depth is {depth}")
     links = getLinks(title)
+
     if targetPagePresent(targetPage, links):
         return True
     elif depth < maxDepth:
@@ -18,28 +24,55 @@ def findTargetPage(title: str, targetPage: str, depth: int):
         )
 
 
-def getLinks(title: str):
-    with open("resources/header.txt", "r") as file:
-        global userAgentString
-        userAgentString = file.read().strip()
-        headers = {"User-Agent": userAgentString}
-
-    params = (
-        ("action", "query"),
-        ("generator", "links"),
-        ("titles", title),
-        ("format", "json"),
-        ("formatversion", "2"),
-    )
-
+# Add a type annotation for params?
+def makeRequest(params, headers):
     url = "https://www.wikipedia.org/w/api.php"
     r = requests.get(url, headers=headers, params=params)
     rJson = r.json()
-    return rJson["query"]["pages"]
+    return rJson
+
+
+def getLinks(title: str):
+    queue = []
+    result = makeRequest(
+        (
+            ("action", "query"),
+            ("generator", "links"),
+            ("titles", title),
+            ("format", "json"),
+            ("formatversion", "2"),
+            ("gpllimit", "max"),
+        ),
+        headers,
+    )
+
+    addToQueue(queue, result["query"]["pages"])
+
+    while "continue" in result.keys():
+        result = makeRequest(
+            (
+                ("action", "query"),
+                ("generator", "links"),
+                ("titles", title),
+                ("format", "json"),
+                ("formatversion", "2"),
+                ("gplcontinue", result["continue"]["gplcontinue"]),
+                ("gpllimit", "max"),
+            ),
+            headers,
+        )
+        addToQueue(queue, result["query"]["pages"])
+    return queue
 
 
 def targetPagePresent(targetPage: str, links):
     return any(link["title"] == targetPage for link in links)
+
+
+def addToQueue(queue, links):
+    for link in links:
+        if "pageid" in link.keys():
+            queue.append(link)
 
 
 main()
